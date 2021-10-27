4d9a8a3a2f63604e780721daea37ff8d3f962d2a
Yelim <fla99916@gmail.com>
2021-10-27 18:02:24 +0200

    Adding files

---
+++ WeatherObservationStation3.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+*/
+SELECT distinct (city)
+FROM STATION 
+where ID % 2 = 0;
---
+++ WeatherObservationStation4.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+*/
+
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY))
+FROM STATION
